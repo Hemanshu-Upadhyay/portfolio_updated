dcf2c7f0c2e8ab277d694d0377cad9414186bfb8
Hemanshu-Upadhyay <upadhyayhri@gmail.com>
2024-12-09 13:50:14 +0530

    performance issues fix

--- src/components/CanvasLoader.jsx
+++ src/components/CanvasLoader.jsx
@@ -1,29 +1,45 @@
-import { Html, useProgress } from '@react-three/drei';
+import React, { Suspense } from "react";
+import { Html, useProgress } from "@react-three/drei";
 
-const CanvasLoader = () => {
-    const { progress } = useProgress();
-    return (
-        <Html
-            as="div"
-            center
-            style={{
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-                flexDirection: 'column',
-            }}>
-            <span className="canvas-loader"></span>
-            <p
-                style={{
-                    fontSize: 14,
-                    color: '#F1F1F1',
-                    fontWeight: 800,
-                    marginTop: 40,
-                }}>
-                {progress !== 0 ? `${progress.toFixed(2)}%` : 'Loading...'}
-            </p>
-        </Html>
-    );
+const CanvasLoader = ({
+  loaderClassName = "canvas-loader",
+  textStyle = {},
+  containerStyle = {},
+}) => {
+  const { progress } = useProgress();
+
+  return (
+    <Html
+      as="div"
+      center
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+        ...containerStyle,
+      }}
+    >
+      <span className={loaderClassName}></span>
+      <p
+        style={{
+          fontSize: 14,
+          color: "#F1F1F1",
+          fontWeight: 800,
+          marginTop: 40,
+          ...textStyle,
+        }}
+      >
+        {progress.toFixed(2) !== "0.00"
+          ? `${progress.toFixed(2)}%`
+          : "Loading..."}
+      </p>
+    </Html>
+  );
+};
+
+export const Canvas3DLoader = ({ children, fallback = <CanvasLoader /> }) => {
+  return <Suspense fallback={fallback}>{children}</Suspense>;
 };
 
 export default CanvasLoader;
